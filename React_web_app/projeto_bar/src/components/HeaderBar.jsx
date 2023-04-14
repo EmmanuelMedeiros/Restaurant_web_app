0f4ff4e27dcba6432a60d903eb401e33d4ffc417
Emmanuel <emmanuelmedeiros05@gmail.com>
2023-04-14 20:41:09 -0300

    Some changes in frontEnd

--- React_web_app/projeto_bar/src/components/HeaderBar.jsx
+++ React_web_app/projeto_bar/src/components/HeaderBar.jsx
@@ -1,16 +1,14 @@
 import "./styles/headerBar.css"
 
-
 function HeaderBar(props) {
+  
   return (
     <div className="wrapper header_container">
       <ul>
         <li>
-          <h1 className={props.isOpen ? 'green' : 'black'}>B</h1>
+          <h1>B</h1>
         </li>
-        <li onClick={props.onMenuClick}>
-          <span class="material-icons md-18">menu</span>
-        </li>
+        <li onClick={props.onMenuClick}><span class="material-icons md-18">menu</span></li>
       </ul>
     </div>
   );
--- React_web_app/projeto_bar/src/components/MenuBar.jsx
+++ React_web_app/projeto_bar/src/components/MenuBar.jsx
@@ -4,14 +4,28 @@ import { useEffect, useState } from 'react'
 
 function MenuBar() {
     const [isOpen, setOpen] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
   
     return (
       <div>
         <div className={isOpen ? 'hide_it' : 'show_it'}>
-          <HeaderBar isOpen={isOpen} onMenuClick={() => setOpen(true)} />
+          <HeaderBar onMenuClick={() => setOpen(true)} />
         </div>
-        <div className="wrapper menu_container">
-          <h1 onClick={() => setOpen(false)}>I'am header bar</h1>
+        <div className={isOpen ? 'wrapper menu_container' : 'hide_menu'}>
+
+          <ul className="top_bar">
+            <li><h1>B</h1></li>
+            <li onClick={() => setOpen(false)}><span class='material-icons md-18'>close</span></li>
+          </ul>
+
+          <ul className="items">
+            <li onClick={menuOpen ? () => setMenuOpen(false) : () => setMenuOpen(true)} ><h2>+ Menu</h2></li>
+            <li className={menuOpen ? "menu_itens" : "menu_closed"}>Pratos completos</li>
+            <li className={menuOpen ? "menu_itens" : "menu_closed"}>Tira-gostos</li>
+            <li className={menuOpen ? "menu_itens" : "menu_closed"}>Bebidas</li>
+            <li><h2>Contato</h2></li>
+          </ul>
+          
         </div>
       </div>
     );
